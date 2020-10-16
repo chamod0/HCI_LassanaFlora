9212c658d0ae76b85a75207dcdbf67e59120541c
PasanDewasurendra <pasandewasurendra@gmail.com>
2020-10-16 09:20:16 +0530

    add delete func to wishlist

--- Pasan/assets/js/wishlist.js
+++ Pasan/assets/js/wishlist.js
@@ -33,15 +33,15 @@ $(document).ready(function () {
     var wishlist = JSON.parse(localStorage.getItem('wishlistObj'));
 
     wishlist.forEach((element, index) => {
-        console.log(index);
+        // console.log(index);
         var wishlistItem = "<tr>" +
-            "<td style='display:none;'>"+index+"</td>"+
-            "<td class='pro-thumbnail'><a href='#'><img class='img-fluid' src='"+element.image+"' alt='Product' /></a></td>" +
-            "<td class='pro-title'><a href='#'>"+element.name+"</a></td>" +
-            "<td class='pro-price'><span>"+element.price+"</span></td>" +
+            "<td style='display:none;'>" + index + "</td>" +
+            "<td class='pro-thumbnail'><a href='#'><img class='img-fluid' src='" + element.image + "' alt='Product' /></a></td>" +
+            "<td class='pro-title'><a href='#'>" + element.name + "</a></td>" +
+            "<td class='pro-price'><span>" + element.price + "</span></td>" +
             "<td class='pro-quantity'><span class='text-success'>In Stock</span></td>" +
             "<td class='pro-subtotal'><a href='cart.html' class='sqr-btn'>Add to Cart</a></td>" +
-            "<td class='pro-remove'><button onClick=' deleteWishlistItem("+index+")' class='btn btn-danger'><i class='fa fa-trash-o'></i></button></td>" +
+            "<td class='pro-remove'><button onClick=' deleteWishlistItem(" + index + ")' class='btn btn-danger'><i class='fa fa-trash-o'></i></button></td>" +
             "</tr>";
 
         $("#wishlistTable").find('tbody').append(wishlistItem);
@@ -52,10 +52,14 @@ $(document).ready(function () {
 
 });
 
-
-function deleteWishlistItem(id){
+// remove product form wishlist
+function deleteWishlistItem(id) {
     // alert(id);
 
-    
+    var wishlist = JSON.parse(localStorage.getItem('wishlistObj'));
+    wishlist.splice(id, 1);
+    localStorage.setItem('wishlistObj',JSON.stringify(wishlist)); 
+    console.log(JSON.parse(localStorage.getItem('wishlistObj')));
+
 }
 
